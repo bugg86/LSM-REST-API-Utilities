550d0e0ea6ce31a9dc08ac187a53f858475a3c81
bugg86 <jdfink@uncg.edu>
2022-03-29 19:10:06 -0400

    Version change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lsmrestapiutilities',
-    version='1.1.0',
+    version='1.1.1',
     license='MIT',
     author="Jack Fink",
     author_email='jackfink68@yahoo.com',
